da4fa12d9f6788e7cce2fe3ef9b10387ce2dcb23
Ivan Mihun <vsk76090@gmail.com>
2025-09-15 19:23:32 +0200

    feat(lab1): add console runner

--- lab1/src/main/kotlin/com/example/lab1/Lab1Application.kt
+++ lab1/src/main/kotlin/com/example/lab1/Lab1Application.kt
@@ -1,11 +1,31 @@
 package com.example.lab1
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class Lab1Application
 
 fun main(args: Array<String>) {
-	runApplication<Lab1Application>(*args)
+    runApplication<Lab1Application>(*args)
+}
+
+@Component
+class ArrayMaxRunner : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        println("Enter numbers separated by spaces:")
+
+        val input = readlnOrNull() ?: ""
+        val numbers = input.split(" ")
+            .mapNotNull { it.toIntOrNull() }
+
+        if (numbers.isEmpty()) {
+            println("No valid numbers provided.")
+        } else {
+            val maxNumber = numbers.maxOrNull()
+            println("The maximum number is: $maxNumber")
+        }
+    }
 }
