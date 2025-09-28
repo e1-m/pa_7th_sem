7fb1905b754927831a74a25b73807d221236e5ee
Ivan Mihun <vsk76090@gmail.com>
2025-09-28 19:07:02 +0200

    feat(lab1): make a test change

--- lab1/src/main/kotlin/com/example/lab1/Lab1Application.kt
+++ lab1/src/main/kotlin/com/example/lab1/Lab1Application.kt
@@ -15,7 +15,7 @@ fun main(args: Array<String>) {
 @Component
 class ArrayMaxRunner : CommandLineRunner {
     override fun run(vararg args: String?) {
-        println("Enter numbers separated by spaces:")
+        println("Enter numbers separated by dots:")
 
         val input = readlnOrNull() ?: ""
         val numbers = input.split(" ")
