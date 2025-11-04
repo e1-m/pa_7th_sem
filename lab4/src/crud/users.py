f87592a6fd1ff3e5d3d70fcf0b4f7b7b061ffff4
Ivan Mihun <vsk76090@gmail.com>
2025-11-04 15:21:20 +0100

    refactor(lab4): remove unused `get_by_idp_id` method from CRUD

--- lab4/src/crud/users.py
+++ lab4/src/crud/users.py
@@ -8,6 +8,3 @@ class UserCRUD(Creatable, Retrievable):
 
     async def get_by_email(self, email: str) -> models.User | None:
         return await self._get_one(models.User.email == email)
-
-    async def get_by_idp_id(self, idp_id: str) -> models.User | None:
-        return await self._get_one(models.User.identity_provider_id == idp_id)
